1141cd43e315e84ee6e1164eb677ee2ece27ec7c
Meena Forderer <meena.foerderer@gmail.com>
2022-10-07 12:48:56 +0200

    activate show answer button

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const cardBookmarkBtn = document.querySelector('[data-js="card__bookmark"]');
+const cardAnswer = document.querySelector('[data-js="card__answer"]');
+const showAnswerBtn = document.querySelector('[data-js="card__button-answer"]');
 
-cardBookmarkBtn.addEventListener("click", function (event) {
+//create toggle function on bookmark icon in first card
+
+cardBookmarkBtn.addEventListener("click", (event) => {
   event.target.classList.toggle("bookmark--active");
+});
+
+//activate answer button
+showAnswerBtn.addEventListener("click", () => {
+  if (showAnswerBtn.textContent === "Show answer") {
+    cardAnswer.classList.add("card__answer--active");
+    showAnswerBtn.textContent = "Hide answer";
+  } else {
+    cardAnswer.classList.remove("card__answer--active");
+    showAnswerBtn.textContent = "Show answer";
+  }
 });
