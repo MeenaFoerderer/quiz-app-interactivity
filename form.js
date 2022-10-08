bb89038d947bb93701cb38f76d8f15552cac4dbf
Meena Forderer <meena.foerderer@gmail.com>
2022-10-08 16:20:49 +0200

    finish create card in form.js

--- form.js
+++ form.js
@@ -1,13 +1,9 @@
 const form = document.querySelector('[data-js="form"]');
+const main = document.querySelector("main");
 
-//submit form data
-
+/* ---------- eventlistener to submit form data ----------- */
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log(event.target);
-
-  //const a = event.target.elements.form__question.value;
-  //console.log(a);
 
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
@@ -15,9 +11,14 @@ form.addEventListener("submit", (event) => {
   const question = data.form__question;
   const answer = data.form__answer;
   const tags = data.form__tag;
+
+  addNewCard(question, answer, tags);
 });
 
+/* ----------- function to create new card ---------- */
 function addNewCard(question, answer, tags) {
+  //create card elements
+  const cardList = document.createElement("ul");
   const cardListItem = document.createElement("li");
   const card = document.createElement("article");
   const cardQuestion = document.createElement("h2");
@@ -25,48 +26,54 @@ function addNewCard(question, answer, tags) {
   const cardAnswer = document.createElement("p");
   const cardTagList = document.createElement("ul");
   const cardTagListItem = document.createElement("li");
-  const cardButtonBookmark = document.createElement("");
+  const cardButtonBookmark = document.createElement("div");
+  const cardBookmark = document.createElement("button");
+
+  //add classes to elements
+  cardList.classList.add("card-list");
+  cardListItem.classList.add("card-list__item");
+  card.classList.add("card");
+  cardQuestion.classList.add("card__question");
+  cardButtonAnswer.classList.add("card__button-answer");
+  cardAnswer.classList.add("card__answer");
+  cardTagList.classList.add("card__tag-list");
+  cardTagListItem.classList.add("card__tag-list-item");
+  cardButtonBookmark.classList.add("card__button-bookmark");
+  cardBookmark.classList.add("bookmark");
+
+  //add attributes to elements
+  cardButtonAnswer.setAttribute("data-js", "card__button-answer");
+  cardAnswer.setAttribute("data-js", "card__answer");
+  cardBookmark.setAttribute("data-js", "card__bookmark");
+
+  //add textcontent to elements
+  cardQuestion.textContent = question;
+  cardAnswer.textContent = answer;
+  cardTagListItem.textContent = tags;
+  cardButtonAnswer.textContent = "Show answer";
+  cardBookmark.innerHTML = `<svg
+      class="bookmark__icon"
+      xmlns="http://www.w3.org/2000/svg"
+      viewbox="0 0 24 24"
+      >
+        <path
+        d="M17 3H7c-1.1 0-2 .9-2 2v16l7-3 7 3V5c0-1.1-.9-2-2-2z"
+        />
+    </svg>`;
+
+  //nest/append elements
+  cardList.append(cardListItem);
+  cardListItem.append(card);
+  card.append(
+    cardQuestion,
+    cardButtonAnswer,
+    cardAnswer,
+    cardTagList,
+    cardButtonBookmark
+  );
+  cardTagList.append(cardTagListItem);
+  cardButtonBookmark.append(cardBookmark);
+
+  //append elements below the form
+  main.append(cardList);
 }
-
-/*
-<li class="card-list__item">
-          <article class="card">
-            <h2 class="card__question">
-              In the Kingdom Heart series who provides the english voice for
-              Master Eraqus?
-            </h2>
-            <button
-              class="card__button-answer"
-              type="button"
-              data-js="card__button-answer"
-            >
-              Show answer
-            </button>
-            <p class="card__answer" data-js="card__answer">Mark Hamill</p>
-            <ul class="card__tag-list">
-              <li class="card__tag-list-item">#kingdomheart</li>
-              <li class="card__tag-list-item">#mastereraqus</li>
-              <li class="card__tag-list-item">#english voices</li>
-            </ul>
-            <div class="card__button-bookmark">
-              <button
-                class="bookmark"
-                aria-label="bookmark"
-                type="button"
-                data-js="card__bookmark"
-              >
-                <svg
-                  class="bookmark__icon"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewbox="0 0 24 24"
-                >
-                  <path
-                    d="M17 3H7c-1.1 0-2 .9-2 2v16l7-3 7 3V5c0-1.1-.9-2-2-2z"
-                  />
-                </svg>
-              </button>
-            </div>
-          </article>
-        </li>
-
-*/
